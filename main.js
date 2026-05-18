8c4966a1f103e79453c246c8796d73de3ede2a02
Anant Dongre <anant.dongre@shorelineiot.com>
2026-05-18 14:54:10 +0530

    hardcoded file name

--- main.js
+++ main.js
@@ -743,7 +743,7 @@ async function main() {
         viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
         carousel = false;
     } catch (err) {}
-    const url = params.get("url") || "./test_file.splat";
+    const url = "./test_file.splat";
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
