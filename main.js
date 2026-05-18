2ee6b0ecfec047eec9bd939220d1f4aa6381ba3b
Anant Dongre <anant.dongre@shorelineiot.com>
2026-05-18 14:32:35 +0530

    hardcoded path to splat file

--- main.js
+++ main.js
@@ -743,12 +743,7 @@ async function main() {
         viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
         carousel = false;
     } catch (err) {}
-    const url = new URL(
-        // "nike.splat",
-        // location.href,
-        params.get("url") || "train.splat",
-        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
-    );
+    const url = params.get("url") || "./test_file.splat";
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
